e56854a2110c09c17c67b3d99db6967e86060406
Fidan Hajiyeva <57771443+fi-ji@users.noreply.github.com>
2020-09-26 19:20:51 +0200

    рефакторит код

--- js/main.js
+++ js/main.js
@@ -5,56 +5,55 @@ const mapPins = map.querySelector('.map__pins');
 const pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 const pinFragment = document.createDocumentFragment();
 
+const MOCK = {
+  type: ['palace', 'flat', 'house', 'bungalow'],
+  checkinout: ['12:00', '13:00', '14:00'],
+  features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
+};
+
 map.classList.remove('map--faded');
 
 const getRandomNumBetween = (min, max) => Math.round(Math.random() * (max - min) + min);
 
-const getRandomArr = (item, amount) => {
-  let arr = [];
+const generateRandomData = (mockObj, amount) => {
+  const avatarArr = Array.from({ length: amount }, (element, index) => {
+    let num = index + 1;
+    if (num < 10) {
+      return `0${num}`
+    }
+    return num
+  });
 
-  do {
-    let element = item ? item[Math.floor(Math.random() * amount)] : Math.ceil(Math.random() * amount);
-    arr.push(element);
-
-    arr = arr.filter((elem, index) => arr.indexOf(elem) === index);
-  } while (arr.length < amount);
-
-  return arr;
-};
-
-const generateRandomData = (amount) => {
   const ads = [];
-  const mock = {
-    avatar: getRandomArr(null, amount),
-    type: ['palace', 'flat', 'house', 'bungalow'],
-    checkinout: ['12:00', '13:00', '14:00'],
-    features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-    photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
-  };
 
   for (let i = 0; i < amount; i++) {
-    let ad = {};
+    let locationX = getRandomNumBetween(25, 1175);
+    let locationY = getRandomNumBetween(130, 630);
 
-    ad.author = {
-      avatar: `img/avatars/user0${mock.avatar[i]}.png`
+    let ad = {
+      author: {
+        avatar: `img/avatars/user${avatarArr.splice(getRandomNumBetween(0, avatarArr.length - 1), 1)}.png`
+      },
+      location: {
+        x: locationX,
+        y: locationY
+      },
+      offer: {
+        title: 'Милая, уютная квартирка в центре Токио',
+        address: `${locationX}, ${locationY}`,
+        price: `${getRandomNumBetween(50, 200)}$/ночь`,
+        type: mockObj.type[getRandomNumBetween(0, mockObj.type.length - 1)],
+        rooms: getRandomNumBetween(1, 3),
+        guests: this.rooms < 2 ? getRandomNumBetween(1, 2) : getRandomNumBetween(3, 5),
+        checkin: mockObj.checkinout[getRandomNumBetween(0, mockObj.checkinout.length - 1)],
+        checkout: mockObj.checkinout[getRandomNumBetween(0, mockObj.checkinout.length - 1)],
+        features: mockObj.features.slice(0, getRandomNumBetween(1, mockObj.features.length)),
+        description: 'Уютная, чистая квартира со всей необходимой мебелью и техникой. На собственной кухне можно приготовить или разогреть еду. Чай, кофе, каша, макароны и масло есть всегда. До центра около 10 минут пешком.',
+        photos: mockObj.photos.slice(0, getRandomNumBetween(1, mockObj.photos.length))
+      }
     };
-    ad.location = {
-      x: getRandomNumBetween(25, 1175),
-      y: getRandomNumBetween(130, 630)
-    };
-    ad.offer = {
-      title: 'Милая, уютная квартирка в центре Токио',
-      address: `${ad.location.x}, ${ad.location.y}`,
-      price: `${getRandomNumBetween(50, 200)}$/ночь`,
-      type: mock.type[getRandomNumBetween(0, mock.type.length - 1)],
-      rooms: getRandomNumBetween(1, 3),
-      guests: ad.rooms < 2 ? getRandomNumBetween(1, 2) : getRandomNumBetween(3, 5),
-      checkin: mock.checkinout[getRandomNumBetween(0, mock.checkinout.length - 1)],
-      checkout: mock.checkinout[getRandomNumBetween(0, mock.checkinout.length - 1)],
-      features: getRandomArr(mock.features, getRandomNumBetween(1, 6)),
-      description: 'Уютная, чистая квартира со всей необходимой мебелью и техникой. На собственной кухне можно приготовить или разогреть еду. Чай, кофе, каша, макароны и масло есть всегда. До центра около 10 минут пешком.',
-      photos: getRandomArr(mock.photos, getRandomNumBetween(1, 3))
-    };
+
     ads.push(ad);
   }
   return ads;
@@ -78,7 +77,7 @@ const fillFragment = (frag, list, func) => {
   return frag;
 };
 
-const adsList = generateRandomData(8);
+const adsList = generateRandomData(MOCK, 8);
 
 fillFragment(pinFragment, adsList, renderAds);
 
