20f782297a809613798135198a26c2ea7fb2eb56
Fidan Hajiyeva <57771443+fi-ji@users.noreply.github.com>
2020-10-23 15:36:42 +0200

    настраивает webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './js/load.js',
+    './js/util.js',
+    './js/map.js',
+    './js/form.js',
+    './js/move.js',
+    './js/pin.js',
+    './js/card.js',
+    './js/filter.js',
+    './js/render.js',
+    './js/main.js',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname),
+    iife: true
+  },
+  devtool: false
+};
